1518c8883f6a590af544da023351d24385278e46
Emma Wright <emmwright9@gmail.com>
2025-03-18 14:11:41 -0700

    Add javascript assignment 4 files

---
+++ assignments/assignment-4/hello.js
@@ -0,0 +1,9 @@
+// assignment 4
+
+const greeting = "JavaScript";
+const createMessage = (name) => {
+    return `Hello ${name}, welcome to ${greeting}`;
+    };
+
+    console.log(createMessage("Emma"));
+    
