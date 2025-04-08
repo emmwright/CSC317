a6491a55a23f9f37c25bd3e86ecd06a6b20d8c12
Emma Wright <emmwright9@gmail.com>
2025-04-08 15:46:40 -0700

    html buttons

---
+++ assignments/assignment-4/javascript.js
@@ -0,0 +1,28 @@
+// assignment 4
+
+const greeting = "JavaScript";
+const createMessage = (name) => {
+    return `Hello ${name}, welcome to ${greeting}`;
+    };
+
+    console.log(createMessage("Emma"));
+    
+
+    //adding + element
+
+    //subtract - element
+
+    //multiply * element
+
+    //divide / element
+
+    //percentage % calculation
+
+    //clear (AC)
+
+    //positive/negative +/- toggle
+
+    //decimal point .
+
+    //equals =
+
